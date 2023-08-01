58a10e43cd8b1c5120f381227d1f0f325459373d
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2023-08-01 17:15:44 -0400

    add Barbara Morris expansion buildable by Bob with evf

--- testfixtures/hll/Rules.mk
+++ testfixtures/hll/Rules.mk
@@ -1,3 +1,5 @@
 LITINERR.PGM: literalInMessage.pgm.rpgle
 SQLRPGLE.PGM: sqlrpgle.pgm.sqlrpgle
 SQLCPP.PGM: sqlcpp.pgm.sqlcpp
+EXPANDMAIN.PGM: expandmain.pgm.sqlrpgle
+EXPAN2MAIN.PGM: expan2main.pgm.sqlrpgle
---
+++ testfixtures/hll/expan2main.pgm.sqlrpgle
@@ -0,0 +1,8 @@
+**free
+
+/copy expan2cpy1
+
+dcl-s x char(10) inz(x'1');  // error, bad hex literal
+
+return;
+
