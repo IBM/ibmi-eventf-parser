1465b96029b3c61777bdf9ef6d1db13cd6359729
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-08-02 16:07:40 -0400

    fix current process
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/ExpansionProcessor.ts
+++ src/ExpansionProcessor.ts
@@ -24,7 +24,7 @@ import { TimestampRecord } from "./record/TimestampRecord";
 export class ExpansionProcessor implements IProcessor {
 	private PRE_COMPILE_PROCESSOR_ID = "999";
 
-	protected currentProcessor: ProcessorBlock;
+	protected currentProcessor: ProcessorBlock | undefined;
 
 	// Flags any errors that occur during processing and disables the processor if needed
 	protected postProcessingNeeded: boolean = true;
@@ -42,26 +42,23 @@ export class ExpansionProcessor implements IProcessor {
 	// If true, temporary output members are involved, and mappings may be needed (unless not supported by pre-compilers)
 	protected _containsPreCompileProcessorBlocks: boolean = false;
 
-	constructor() {
-		const record = new ProcessorRecord('', '', '');
-		this.currentProcessor = new ProcessorBlock(record);
-	}
+	constructor() { }
 
 	public processErrorRecord(record: ErrorInformationRecord) {
-		this.currentProcessor.addErrorInformation(record);
+		this.currentProcessor!.addErrorInformation(record);
 	}
 
 	public processExpansionRecord(record: ExpansionRecord) {
 		// this.containsExpansionEvents = true;
-		this.currentProcessor.setContainsExpansionEvents(true);
-		this.currentProcessor.getMappingTable().addExpansionRecord(record);
+		this.currentProcessor!.setContainsExpansionEvents(true);
+		this.currentProcessor!.getMappingTable().addExpansionRecord(record);
 	}
 
 	public processFeedbackCodeRecord(record: FeedbackCodeRecord) { }
 
 	public processFileEndRecord(record: FileEndRecord) {
 		try {
-			this.currentProcessor.closeFile(record);
+			this.currentProcessor!.closeFile(record);
 		} catch (e: any) {
 			this.postProcessingNeeded = false;
 			throw new Error(e.message ? e.message : e);
@@ -73,24 +70,24 @@ export class ExpansionProcessor implements IProcessor {
 		try {
 			const fileID = parseInt(record.getSourceId());
 
-			if (!this.currentProcessor.isProcessorIDZero()) {
+			if (!this.currentProcessor!.isProcessorIDZero()) {
 				if (fileID > 1) {
-					if (!this.currentProcessor.getOutputFile()) {
-						this.currentProcessor.setOutputFile(record);
+					if (!this.currentProcessor!.getOutputFile()) {
+						this.currentProcessor!.setOutputFile(record);
 						return;
 					}
-				} else if (fileID === 1 && !this.currentProcessor.getInputFile()) {
-					this.currentProcessor.setInputFile(record);
+				} else if (fileID === 1 && !this.currentProcessor!.getInputFile()) {
+					this.currentProcessor!.setInputFile(record);
 				}
 
-				this.currentProcessor.addFile(record);
+				this.currentProcessor!.addFile(record);
 			} else {
 				// We need to keep track of all files in the compiler processor block to be able to get the location for files
-				if (fileID === 1 && !this.currentProcessor.getInputFile()) {
-					this.currentProcessor.setInputFile(record);
+				if (fileID === 1 && !this.currentProcessor!.getInputFile()) {
+					this.currentProcessor!.setInputFile(record);
 				}
 
-				this.currentProcessor.getMappingTable().addFileToFileTable(record);
+				this.currentProcessor!.getMappingTable().addFileToFileTable(record);
 			}
 		} catch (e: any) {
 			this.postProcessingNeeded = false;
@@ -110,8 +107,8 @@ export class ExpansionProcessor implements IProcessor {
 		}
 
 		try {
-			if (this.currentProcessor) {
-				this.currentProcessor.processorEnded();
+			if (this.currentProcessor!) {
+				this.currentProcessor!.processorEnded();
 			}
 		} catch (e: any) {
 			this.postProcessingNeeded = false;
@@ -127,8 +124,8 @@ export class ExpansionProcessor implements IProcessor {
 			this._noProcessorEventsYet = false;
 		}
 
-		newProcessor.setPreviousProcessor(this.currentProcessor);
-		this.currentProcessor = newProcessor;
+		newProcessor.setPreviousProcessor(this.currentProcessor!);
+		this.currentProcessor! = newProcessor;
 	}
 
 
@@ -147,11 +144,11 @@ export class ExpansionProcessor implements IProcessor {
 	}
 
 	public doPostProcessing(): boolean {
-		if (!this.postProcessingNeeded || !this._containsPreCompileProcessorBlocks || !this.determineAndSetMappingSupport(this.currentProcessor)) {
+		if (!this.postProcessingNeeded || !this._containsPreCompileProcessorBlocks || !this.determineAndSetMappingSupport(this.currentProcessor!)) {
 			return true;
 		}
 
-		this.currentProcessor.resolveFileNamesForAllErrors();
+		this.currentProcessor!.resolveFileNamesForAllErrors();
 		return false;
 	}
 
@@ -264,16 +261,16 @@ export class ExpansionProcessor implements IProcessor {
 	}
 
 	public getAllErrors(): ErrorInformationRecord[][] {
-		if (this.currentProcessor) {
-			return this.currentProcessor.getAllProcessorErrors();
+		if (this.currentProcessor!) {
+			return this.currentProcessor!.getAllProcessorErrors();
 		}
 
 		return [];
 	}
 
 	public getAllFileIDRecords(): FileIDRecord[] {
-		if (this.currentProcessor) {
-			return this.currentProcessor.getMappingTable().getAllFileIDRecords();
+		if (this.currentProcessor!) {
+			return this.currentProcessor!.getMappingTable().getAllFileIDRecords();
 		} else {
 			return [];
 		}
