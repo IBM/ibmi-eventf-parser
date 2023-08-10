64999ac96c5abebc2fe635292ed169a5cc31b228
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-08-10 11:26:52 -0400

    add to exports
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@
  */
 
 export { Parser } from "./Parser";
+export { ErrorInformationRecord } from "./record/ErrorInformationRecord";
+export { ISequentialFileReader } from "./ISequentialFileReader";
 export { IMarkerCreator } from "./IMarkerCreator";
+export { IProcessor } from "./IProcessor";
